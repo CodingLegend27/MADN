bebd329ac8fe677ab83b26610c47fadbe3896ebe
SquareZzz <squareZz18@gmail.com>
2020-07-09 15:01:01 +0200

    sDAF

--- CONTROLLER.java
+++ CONTROLLER.java
@@ -1,3 +1,14 @@
+import java.awt.event.ActionListener;
+
 public class CONTROLLER {
-    
+
+    private VIEW view;
+    private MODEL model;
+
+    public CONTROLLER(VIEW view, MODEL model){
+
+        this.view = view;
+        this.model = model;
+    }
+
 }
--- VIEW.java
+++ VIEW.java
@@ -1,10 +1,28 @@
-public class VIEW{
-
-    
-
-    public void main(String[] args){
+import java.awt.Color;
+import java.awt.event.ActionListener;
 
 
+import javax.swing.*;
+public class VIEW extends JFrame
+{
+    private JTextField text = new JTextField(10);
+
+    public VIEW(){
+
+        JPanel panel = new JPanel();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000,1000);
+
+        text.setForeground(Color.black);
+        panel.add(text);
+
+        this.add(panel);
+        
+        this.show();
     }
 
+    public static void main(String[] args){
+        VIEW v = new VIEW();
+        
+    }
 }
