0319b88add6cdd02aaa57341e263ea5e5202a736
WAFFLERC27@schule.gym-beilngries.tsc <WAFFLERC27@schule.gym-beilngries.tsc>
2020-07-16 08:38:00 +0200

    new class Monitor for Input & Output for the user

---
+++ view/MONITOR.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JPanel;
+
+/**
+ * Klasse zum Anzeigen der gewürfelten Ergebnisse, Ein- und Ausgabemöglichkeit für den Benutzer
+ */
+public class MONITOR extends JPanel {
+    
+    
+
+    public MONITOR() {
+        super();
+        
+    }
+}
